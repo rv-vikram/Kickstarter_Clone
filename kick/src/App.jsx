fe15a19be08fd96a886abc52cb320262afeeae0d
Thirupathichalla-arch <91534651+Thirupathichalla-arch@users.noreply.github.com>
2022-01-20 21:07:53 +0530

    compite

---
+++ kick/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Todo from './components/Index1.jsx'
+function App() {
+  return (
+    <div>
+      < Todo />
+      
+    </div>
+  );
+}
+
+export default App;
