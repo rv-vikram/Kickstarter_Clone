faafe906aaa818b1c6b2ed8081f8fa3ea696ce8e
Kalpit Sharma <sharmakalpit78@gmail.com>
2022-02-11 10:37:55 +0530

    init

--- src/Components/Detailsfds.jsx
+++ src/Components/Detailsfds.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { Header } from "./Header";
-import { Navbar } from "./Navbar";
+// import { Navbar } from "./Navbar";
 
 export const Details = () => {
 
